58af3046ef03f42d30b34147228ff79b839c13d8
TextError <savinraul1983@yahoo.com>
2019-08-03 18:57:06 +0100

    more responsive css

--- client/src/components/dashboard/Title.js
+++ client/src/components/dashboard/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => {
   return (
-    <div className='row no-gutters mb-5'>
+    <div className='row no-gutters mb-5 dashboard-title'>
       <div className='col col-sm-8 col-md-10 col-lg-6 m-auto'>
         <div className='dashboard-title m-auto text-center'>
           <h1 className='text-primary'>CRUD</h1>
