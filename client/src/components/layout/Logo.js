ffe2bc10438b3928fab23eaabd9d5690832e4bdf
TextError <savinraul1983@yahoo.com>
2019-12-31 10:13:16 +0200

    NPM update

--- client/src/components/layout/Logo.js
+++ client/src/components/layout/Logo.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// Tilt
-import Tilt from 'react-tilt'
-
 // Assets
 import logo from '../../assets/logo.png';
 
  const Logo = () => {
   return (
-    <Tilt className="Tilt" options={{ max : 25 }}>
-      <div className="Tilt-inner">
-        <div className='logo'>
-          <Link to='/'><img src={logo} alt='logo' /></Link>
-        </div>
-      </div>
-    </Tilt>
+    <div className='logo'>
+      <Link to='/'><img src={logo} alt='logo' /></Link>
+    </div>
   )
 }
 
