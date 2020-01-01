dccd640c58db66fdac04193d6aa43421bc61dd9c
TextError <savinraul1983@yahoo.com>
2020-01-01 13:57:58 +0200

    Keep position for posts

--- client/src/redux/selectors/post.js
+++ client/src/redux/selectors/post.js
@@ -1,11 +1,12 @@
 import { createSelector } from 'reselect';
+import doSort from './utils/doSort';
 
 const select_post_state = state => state.post.posts;
 const select_isLoading_state = state => state.loading.post;
 
 export const state_posts = createSelector(
   [select_post_state],
-  state => state
+  state => doSort(state)
 );
 
 export const state_delete_isLoading = createSelector(
---
+++ client/src/redux/selectors/utils/doSort.js
@@ -0,0 +1,3 @@
+const doSort = items => items.sort((a, b) => a.position - b.position);
+
+export default doSort;
