64da3d8f651e939e9dfd9baeb5b11e696970bd6a
TextError <savinraul1983@yahoo.com>
2020-01-01 14:31:32 +0200

    Prepare for heroku

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='home d-flex justify-content-center align-items-center flex-column'>
-      <h2 className='mb-5'>A simple CRUD application</h2>
+      <h2 className='mb-5 text-center'>A simple CRUD application</h2>
       <div className='d-flex'>
         <Link to='/register'><h3>Register</h3></Link>
         <h3 className='ml-2 mr-2'>or</h3>
